f9878d214ed76419d45a2a6a8dcdd82598b4ae1d
raiden808 <raiden.nalasa@gmail.com>
2021-10-04 14:56:12 +0800

    feat: moved db to knex file

---
+++ knexfile.ts
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+export const development = {
+    client: 'mysql',
+    connection: {
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+        port: process.env.PORT
+    }
+}
--- src/models/todo.ts
+++ src/models/todo.ts
@@ -1,6 +1,8 @@
 import { Model } from 'objection';
 import { knex } from '../database';
 
+// Give the knex instance to objection.
+// Model.knex(knex);
 Model.knex(knex)
 
 class Todo extends Model {
