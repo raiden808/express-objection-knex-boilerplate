87ba8081b874e0c28d26ca5a28ac680f358ef8e9
raiden808 <raiden.nalasa@gmail.com>
2021-11-18 21:11:37 +0800

    feat: set up body parser for JSON

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express from "express";
 import allRoutes from "./routes";
+var bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// parse application/json
+app.use(bodyParser.json())
 
 
 app.use('/v1', allRoutes);
+
+
 
 app.listen(3000, () => {
     console.log('Server is up on 3000')
