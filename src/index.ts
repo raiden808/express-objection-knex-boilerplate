fb684315ee0e570e4f4da5310e8ef0f252512f3a
raiden808 <raiden.nalasa@gmail.com>
2022-01-18 10:51:23 +0800

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import allRoutes from "./routes";
+import cors from 'cors';
+
 var bodyParser = require('body-parser')
 const app = express()
+
+app.use(cors())
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+
 
 // parse application/json
 app.use(bodyParser.json())
